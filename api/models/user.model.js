71c9c872512f913a3acfe7b1d731c5cb3a7b9059
Shivamk777 <shivampatel17119@gmail.com>
2024-07-14 20:38:54 +0530

    make the profile page private

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -15,9 +15,12 @@ const userSchema = new mongoose.Schema(
       password: {
         type: String,
         required: true,
-      } 
+      },
 
-    
+      password: {
+        type: String,
+        default: "https://media.istockphoto.com/id/2098359215/photo/digital-marketing-concept-businessman-using-laptop-with-ads-dashboard-digital-marketing.jpg?s=1024x1024&w=is&k=20&c=q6RTyRcP6Lli25bBXmKz3F3sIAVSu5PthcuOiAniHzE="
+      },
 
     }, { timestamps: true } );
     
